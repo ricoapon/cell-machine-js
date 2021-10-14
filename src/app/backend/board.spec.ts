c6f712836d747fd1f1426b01ca457f1c304c5849
Rico Apon <rico.apon@gmail.com>
2021-10-14 22:53:21 +0200

    Implement board that contains a grid of cells
    Store build area in the board

---
+++ src/app/backend/board.spec.ts
@@ -0,0 +1,64 @@
+import {Board, Coordinate} from './board';
+import {Direction, Enemy, Generator, Immobile, Mover, Push, Rotator, Slider} from './cells';
+
+describe('backend/Board', () => {
+  it('getCoordinatesOfCellsWithClass() returns only cells with the correct type.', () => {
+    // Given a board with cells of all types.
+    const board = new Board(1, 8);
+    board.setCell(new Mover(Direction.UP), 0, 0);
+    board.setCell(new Generator(Direction.UP), 0, 1);
+    board.setCell(new Rotator(), 0, 2);
+    board.setCell(new Push(), 0, 3);
+    board.setCell(new Slider(Direction.UP), 0, 4);
+    board.setCell(new Immobile(), 0, 5);
+    board.setCell(new Enemy(), 0, 6);
+
+    // When calling the method.
+    const result = board.getCoordinatesOfCellsWithClass(Mover);
+
+    // Then the result should only be the specified types.
+    expect(result).toHaveSize(1);
+    expect(result[0]).toEqual(new Coordinate(0, 0));
+  });
+
+  it('getAllCoordinates() returns all coordinates in the right order.', () => {
+    const board = new Board(2, 3);
+    const result = board.getAllCoordinates();
+    expect(result).toHaveSize(6);
+    expect(result[0]).toEqual(new Coordinate(0, 0));
+    expect(result[1]).toEqual(new Coordinate(0, 1));
+    expect(result[2]).toEqual(new Coordinate(0, 2));
+    expect(result[3]).toEqual(new Coordinate(1, 0));
+    expect(result[4]).toEqual(new Coordinate(1, 1));
+    expect(result[5]).toEqual(new Coordinate(1, 2));
+  });
+
+  it('containsCoordinate() works correctly.', () => {
+    const board = new Board(2, 3);
+    expect(board.containsCoordinate(null)).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(-1, 2))).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(1, -2))).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(0, 0))).toBeTrue();
+    expect(board.containsCoordinate(new Coordinate(2, 3))).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(2, 2))).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(1, 3))).toBeFalse();
+    expect(board.containsCoordinate(new Coordinate(1, 2))).toBeTrue();
+  });
+
+  it('setCell() and getSell() methods with overrides work correctly.', () => {
+    const board = new Board(1, 2);
+    const firstCell = new Enemy();
+    const secondCell = new Immobile();
+    board.setCell(firstCell, new Coordinate(0, 0));
+    board.setCell(secondCell, 0, 1);
+    expect(board.getCell(0, 0)).toEqual(firstCell);
+    expect(board.getCell(new Coordinate(0, 1))).toEqual(secondCell);
+  });
+
+  it('setBuildArea() and getBuildArea() works.', () => {
+    const board = new Board(4, 4);
+    board.setBuildArea(new Coordinate(1, 1), new Coordinate(3, 3));
+    expect(board.getBuildArea().topLeftCoordinate).toEqual(new Coordinate(1, 1));
+    expect(board.getBuildArea().bottomRightCoordinate).toEqual(new Coordinate(3, 3));
+  });
+});
---
+++ src/app/backend/board.ts
@@ -0,0 +1,108 @@
+import {Cell} from './cells';
+
+export class Coordinate {
+  public readonly x: number;
+  public readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class RectangularArea {
+  public readonly topLeftCoordinate: Coordinate;
+  public readonly bottomRightCoordinate: Coordinate;
+
+  constructor(topLeftCoordinate: Coordinate, bottomRightCoordinate: Coordinate) {
+    this.topLeftCoordinate = topLeftCoordinate;
+    this.bottomRightCoordinate = bottomRightCoordinate;
+  }
+}
+
+export class Board {
+  public readonly width; public readonly height;
+  /** Rectangular grid with corners `grid[0][0]` (top-left) and `grid[width-1][height-1]` (bottom-right). */
+  private readonly grid: Cell[][];
+  /** The editable area of the board where the player can move around cells. */
+  private buildArea: RectangularArea;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.grid = new Array<Array<Cell>>();
+    for (let i = 0; i < width; i++) {
+      this.grid[i] = new Array<Cell>();
+    }
+    this.buildArea = new RectangularArea(new Coordinate(0, 0), new Coordinate(0, 0));
+  }
+
+  public getCell(coordinate: Coordinate): Cell;
+  public getCell(x: number, y: number): Cell;
+  public getCell(x: number | Coordinate, y?: number): Cell {
+    if (y == null) {
+      // We called the method with x the coordinate. Change into x,y calling.
+      const coordinate = x as Coordinate;
+      y = coordinate.y;
+      x = coordinate.x;
+    }
+    return this.grid[x as number][y];
+  }
+
+  public setCell(cell: Cell, coordinate: Coordinate): void;
+  public setCell(cell: Cell, x: number, y: number): void;
+  public setCell(cell: Cell, x: number | Coordinate, y?: number): void {
+    if (y == null) {
+      // We called the method with x the coordinate. Change into x,y calling.
+      const coordinate = x as Coordinate;
+      y = coordinate.y;
+      x = coordinate.x;
+    }
+    this.grid[x as number][y] = cell;
+  }
+
+  /**
+   * Returns a list of all coordinates that are on the board, where the order of the coordinates is from the top left to the bottom right,
+   * top to bottom.
+   */
+  public getAllCoordinates(): Array<Coordinate> {
+    const result = new Array<Coordinate>();
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        result.push(new Coordinate(i, j));
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Returns whether the coordinate exists on the board.
+   * @param coordinate The coordinate to check.
+   */
+  public containsCoordinate(coordinate: Coordinate): boolean {
+    return coordinate != null && !(coordinate.x < 0 || coordinate.x >= this.width || coordinate.y < 0 || coordinate.y >= this.height);
+  }
+
+  /**
+   * Returns a list of coordinates of all the cells that are of the given class.
+   * @param cellClass The class of the cell.
+   */
+  public getCoordinatesOfCellsWithClass(cellClass: any): Array<Coordinate> {
+    const result = new Array<Coordinate>();
+    for (const coordinate of this.getAllCoordinates()) {
+      if (this.getCell(coordinate) instanceof cellClass) {
+        result.push(coordinate);
+      }
+    }
+    return result;
+  }
+
+  public getBuildArea(): RectangularArea {
+    return this.buildArea;
+  }
+
+  public setBuildArea(topLeftCoordinate: Coordinate, bottomRightCoordinate: Coordinate): void {
+    this.buildArea = new RectangularArea(topLeftCoordinate, bottomRightCoordinate);
+  }
+}
+
