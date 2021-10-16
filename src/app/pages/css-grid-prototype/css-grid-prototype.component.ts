2f2e89622bdb6e7051c322e5e0af9f387659e40a
Rico Apon <rico.apon@gmail.com>
2021-10-16 14:32:38 +0200

    Make grid size variable based on imported board

--- src/app/pages/css-grid-prototype/css-grid-prototype.component.ts
+++ src/app/pages/css-grid-prototype/css-grid-prototype.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Game} from '../../backend/game';
-import {Coordinate} from '../../backend/board';
+import {Board, Coordinate} from '../../backend/board';
 import {Cell, CellType, CellWithDirection, Direction} from '../../backend/cells';
 
 @Component({
@@ -26,7 +26,14 @@ export class CssGridPrototypeComponent implements OnInit {
   }
 
   initializeGame(): void {
-    this.game = new Game(6, 6);
+    // Determine the size of the game before reading the string.
+    // A bit weird to do so, but that is just a prototype.
+    const boardAsStringSplit = this.boardAsStringInput.split('/');
+    const size = boardAsStringSplit[1];
+
+    const sizeMatch = size.match('^(\\d+),(\\d+)$');
+
+    this.game = new Game(+sizeMatch[1], +sizeMatch[2]);
     this.game.setBoardFromString(this.boardAsStringInput);
   }
 
@@ -84,4 +91,10 @@ export class CssGridPrototypeComponent implements OnInit {
     }
   }
 
+  determineGridSize(): any {
+    return {
+      'grid-template-columns': 'repeat(' + this.game.getBoard().width + ', 50px)',
+      'grid-template-rows': 'repeat('  + this.game.getBoard().height + ', 50px)',
+    };
+  }
 }
