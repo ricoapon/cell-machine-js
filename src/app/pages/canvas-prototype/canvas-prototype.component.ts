fdd4702c42b8238b1b2f2365b9645625adfa86a2
Rico Apon <rico.apon@gmail.com>
2021-10-16 21:05:44 +0200

    For easier testing use different variable for showing current board as string

--- src/app/pages/canvas-prototype/canvas-prototype.component.ts
+++ src/app/pages/canvas-prototype/canvas-prototype.component.ts
@@ -9,21 +9,23 @@ import {CanvasPrototypeManager} from './canvas-prototype-manager';
 export class CanvasPrototypeComponent implements OnInit {
   canvasPrototypeManager: CanvasPrototypeManager;
   boardAsStringInput = '1/6,6/0,0-2,2/1x1MD2x1ML1x1R29x';
+  currentBoardAsString = '1/6,6/0,0-2,2/1x1MD2x1ML1x1R29x';
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.canvasPrototypeManager = new CanvasPrototypeManager('game-canvas', 50, 6);
-    this.canvasPrototypeManager.initializeFromString(this.boardAsStringInput);
+    this.initializeGame();
   }
 
   doStep(): void {
     this.canvasPrototypeManager.doStep();
-    this.boardAsStringInput = this.canvasPrototypeManager.getBoardAsString();
+    this.currentBoardAsString = this.canvasPrototypeManager.getBoardAsString();
   }
 
   initializeGame(): void {
     this.canvasPrototypeManager.initializeFromString(this.boardAsStringInput);
+    this.currentBoardAsString = this.boardAsStringInput;
   }
 }
