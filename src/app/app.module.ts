9cb2b943548627bf1a89fef2f3a9f84957c23782
Rico Apon <rico.apon@gmail.com>
2021-10-16 15:43:40 +0200

    Add image creator that is loaded on startup to avoid async stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,7 @@ import { MenuScreenComponent } from './pages/menu-screen/menu-screen.component';
 import { CanvasPrototypeComponent } from './pages/canvas-prototype/canvas-prototype.component';
 import { CssGridPrototypeComponent } from './pages/css-grid-prototype/css-grid-prototype.component';
 import {FormsModule} from '@angular/forms';
+import {CanvasCellImageCreator} from './pages/canvas-prototype/canvas-cell-image-creator';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import {FormsModule} from '@angular/forms';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide: APP_INITIALIZER, useFactory: CanvasCellImageCreator.initializeCanvasCellCreator, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/canvas-prototype/canvas-cell-image-creator.ts
@@ -0,0 +1,66 @@
+import {fabric} from 'fabric';
+import {CellType} from '../../backend/cells';
+import {Image} from 'fabric/fabric-impl';
+
+/**
+ * Images are loaded asynchronously. To avoid having to deal with this issue, this class will load all the images into memory.
+ * We can create new images by copying the already loaded images. This class is initialized when starting up the application.
+ */
+export class CanvasCellImageCreator {
+  private constructor() {
+    this.cells = new Map<CellType, Image>();
+  }
+
+  public static readonly instance: CanvasCellImageCreator = new CanvasCellImageCreator();
+
+  readonly cells: Map<CellType, Image>;
+
+  public static initializeCanvasCellCreator(): () => Promise<void> {
+    return () => {
+      return CanvasCellImageCreator.instance.initializeImageVariables();
+    };
+  }
+
+  async initializeImageVariables(): Promise<void> {
+    return new Promise((resolve) => {
+      let count = 0;
+      const loaded = () => {
+        count++;
+        if (count === 7) {
+          resolve();
+        }
+      };
+
+      this.initializeImageVariable(CellType.ENEMY, 'assets/cells/enemy.svg', loaded);
+      this.initializeImageVariable(CellType.GENERATOR, 'assets/cells/generator.svg', loaded);
+      this.initializeImageVariable(CellType.IMMOBILE, 'assets/cells/immobile.svg', loaded);
+      this.initializeImageVariable(CellType.MOVER, 'assets/cells/mover.svg', loaded);
+      this.initializeImageVariable(CellType.PUSH, 'assets/cells/push.svg', loaded);
+      this.initializeImageVariable(CellType.ROTATOR, 'assets/cells/rotator.svg', loaded);
+      this.initializeImageVariable(CellType.SLIDER, 'assets/cells/slider.svg', loaded);
+    });
+  }
+
+  private initializeImageVariable(cellType: CellType, url: string, callback: () => void): void {
+    fabric.Image.fromURL(url, (img: Image) => {
+      this.cells.set(cellType, img);
+      callback();
+    });
+  }
+
+  /**
+   * Create image for the canvas for which you only need to set the left and top property.
+   * @param cellType The type of the cell.
+   * @param gridCellSizeInPx The size of the grid.
+   */
+  public createImage(cellType: CellType, gridCellSizeInPx: number): Image {
+    const image = fabric.util.object.clone(this.cells.get(cellType));
+    // Images get their own width and height. To make sure it will fit in a cell with the correct size, we need to scale.
+    image.scaleToWidth(gridCellSizeInPx);
+    image.scaleToHeight(gridCellSizeInPx);
+    image.set({
+      hasControls: false
+    });
+    return image;
+  }
+}
