0f5722cb182f3355d621dbbbcf45289c6861a6a0
Rico Apon <rico.apon@gmail.com>
2021-10-17 10:54:16 +0200

    Dynamically load levels in the levels page

--- src/app/pages/level/level.component.spec.ts
+++ src/app/pages/level/level.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LevelComponent } from './level.component';
+import {LevelComponent} from './level.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute, Router} from '@angular/router';
 
 describe('LevelComponent', () => {
+  let router: Router;
+  let route: ActivatedRoute;
   let component: LevelComponent;
   let fixture: ComponentFixture<LevelComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LevelComponent ]
+      declarations: [LevelComponent],
+      imports: [RouterTestingModule.withRoutes([])]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LevelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.get(Router);
+    route = TestBed.get(ActivatedRoute);
+
   });
 
   it('should create', () => {
+    // TODO: figure out how to test this.
+    const spyRoute = spyOn(route.snapshot.paramMap, 'get');
+    spyRoute.and.returnValue('1');
+
     expect(component).toBeTruthy();
   });
 });
--- src/app/pages/level/level.component.ts
+++ src/app/pages/level/level.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {CanvasPrototypeManager} from '../canvas-prototype/canvas-prototype-manager';
+import {LevelStorage} from './level-storage';
 
 @Component({
   selector: 'app-level',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level.component.css']
 })
 export class LevelComponent implements OnInit {
+  levelId: number;
+  helpText: string;
+  canvasPrototypeManager: CanvasPrototypeManager;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.levelId = +params.id);
   }
 
+  ngOnInit(): void {
+    this.initialize(this.levelId);
+  }
+
+  initialize(levelId: number): void {
+    // Make sure the input is actually a number. This is messed up inside tests for some reason.
+    if (isNaN(Number(levelId))) {
+      return;
+    }
+    this.levelId = levelId;
+    const levelStorage = new LevelStorage();
+    const levelData = levelStorage.getLevelData(levelId);
+
+    // Initialize canvas with given level.
+    this.canvasPrototypeManager = new CanvasPrototypeManager('game-canvas', 50);
+    this.canvasPrototypeManager.initializeFromString(levelData.boardAsString);
+    this.helpText = levelData.helpText;
+  }
+
+  doStep(): void {
+    this.canvasPrototypeManager.doStep();
+  }
 }
