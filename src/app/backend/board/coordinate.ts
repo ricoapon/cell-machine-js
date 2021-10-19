02598b36a12f09c00d703a67191eedfa2e35a185
Rico Apon <rico.apon@gmail.com>
2021-10-19 21:19:40 +0200

    Add test for coordinate class

--- src/app/backend/board/coordinate.ts
+++ src/app/backend/board/coordinate.ts
@@ -1,20 +1,20 @@
 export class Coordinate {
-    public readonly x: number;
-    public readonly y: number;
+  public readonly x: number;
+  public readonly y: number;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 
-    equals(otherCoordinate: Coordinate): boolean {
-        if (otherCoordinate == null) {
-            return false;
-        }
-        return this.x === otherCoordinate.x && this.y === otherCoordinate.y;
+  equals(otherCoordinate: Coordinate): boolean {
+    if (otherCoordinate == null) {
+      return false;
     }
+    return this.x === otherCoordinate.x && this.y === otherCoordinate.y;
+  }
 
-    toString(): string {
-        return '(' + this.x + ',' + this.y + ')';
-    }
+  toString(): string {
+    return '(' + this.x + ',' + this.y + ')';
+  }
 }
