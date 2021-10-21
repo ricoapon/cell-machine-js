5986a1f9967487775c7a0960669d2ba832e685b7
Rico Apon <rico.apon@gmail.com>
2021-10-21 16:25:32 +0200

    Implement sandbox for level creation

--- src/app/pages/level-creator/level-creator.component.ts
+++ src/app/pages/level-creator/level-creator.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CanvasSandboxFacade} from '../../canvas/canvas-sandbox-facade';
+import {createCellInstanceFromString} from '../../backend/cells';
+import {Coordinate} from '../../backend/board/coordinate';
 
 @Component({
   selector: 'app-level-creator',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level-creator.component.css']
 })
 export class LevelCreatorComponent implements OnInit {
+  canvasSandboxFacade: CanvasSandboxFacade;
+  onClickFunction = (_: Coordinate) => {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.canvasSandboxFacade = new CanvasSandboxFacade('game-canvas', 50);
+    this.canvasSandboxFacade.initializeFromString('1/10,10/0,0-0,0/1x');
+    this.canvasSandboxFacade.addMouseClickCallback((coordinate) => {
+      this.onClickFunction(coordinate);
+    });
+  }
+
+  setOnClickFunctionToSpawnCell(cellAsString: string): void {
+    this.onClickFunction = (coordinate) => {
+      this.canvasSandboxFacade.setCell(createCellInstanceFromString(cellAsString), coordinate);
+    };
+  }
+
+  setOnClickFunctionToRotate(): void {
+    this.onClickFunction = (coordinate) => {
+      this.canvasSandboxFacade.rotateCellIfItExists(coordinate);
+    };
+  }
+
+  setOnClickFunctionToDelete(): void {
+    this.onClickFunction = (coordinate) => {
+      this.canvasSandboxFacade.setCell(null, coordinate);
+    };
+  }
 }
